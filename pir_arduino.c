b93a8292a3c3481ab768c5579331588120773db4
Aishwarya Parasuram <aishwarya92@berkeley.edu>
2015-03-28 19:50:53 -0700

    Create pir_arduino.c

---
+++ pir_arduino.c
@@ -0,0 +1,49 @@
+
+
+int calibrationTime = 30;        
+
+//the time when the sensor outputs a low impulse
+long unsigned int lowIn;         
+
+//the amount of milliseconds the sensor has to be low 
+//before we assume all motion has stopped
+long unsigned int pause = 5000;  
+
+boolean lockLow = true;
+boolean takeLowTime;  
+
+int pirPin = 2;    //the digital pin connected to the PIR sensor's output
+int ledPin = 13;
+
+
+/////////////////////////////
+//SETUP
+void setup(){
+  Serial.begin(9600);
+  pinMode(pirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(pirPin, LOW);
+
+  //give the sensor some time to calibrate
+  Serial.print("calibrating sensor ");
+    for(int i = 0; i < calibrationTime; i++){
+      Serial.print(".");
+      delay(1000);
+      }
+    Serial.println(" done");
+    Serial.println("SENSOR ACTIVE");
+    delay(50);
+  }
+
+////////////////////////////
+//LOOP
+void loop(){
+
+     if(digitalRead(pirPin) == HIGH){
+     
+         Serial.println("Motion detected");
+         delay(1000);
+       }
+
+    
+  }
