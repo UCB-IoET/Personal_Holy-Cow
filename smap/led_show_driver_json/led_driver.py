b293806d21bdc8ed608e7f804fb0b6c1fc3bda5a
Priyanka <pxnigam@gmail.com>
2015-05-04 15:05:05 -0700

    Finished converting led show driver to handle arrays

--- smap/led_show_driver_json/led_driver.py
+++ smap/led_show_driver_json/led_driver.py
@@ -3,8 +3,11 @@ import urllib2
 import sys
 import msgpack
 import socket
-from smap.archiver.client import RepublishClient
-from smap import driver, util
+import json
+import requests
+from client import RepublishClient
+from smap import util
+from twisted.internet import reactor
 
 archiverurl = 'http://shell.storm.pm:8079'
 archiveraddurl = 'http://shell.storm.pm:8079/add/apikeyhere'
@@ -12,6 +15,9 @@ rate = 1
 subscription_index = 'Path= "/led_driver/1/index"'
 subscription_rgb = 'Path= "/led_driver/1/rgb"'
 subscription_show = 'Path= "/led_driver/1/show"'
+indexpath = 'led_output/1/index'
+rgbpath = '/led_output/1/rgb'
+showpath = '/led_output/1/show'
 # Table to store data to be sent
 table={}
 # Create socket
@@ -52,60 +58,60 @@ def cb_index(points, led_index):
     print "INDEX CALLBACK"
     print "Points: ",points
     print "\nData" , led_index
-	print table
-	index = led_index[0][0][-1]
-    print index
+    print table
+    indexvalue = led_index[0][0][-1]
+    print indexvalue
     now = int(time.time())
-    index[indexpath]['Readings'] = [[now, path]]
+    index[indexpath]['Readings'] = [[now, indexvalue]]
     print(requests.post(archiveraddurl, data = json.dumps(index)))
-	print(requests.post(archiveraddurl, data = json.dumps(rgb)))
-	self.add('/index', curr_time, index)
-	print(led_index[0][0][-2])
-	if (led_index[0][0][-2] not in table.keys()):
-		table[led_index[0][0][-2]] = {}
-	table[led_index[0][0][-2]][0] = index
+    print(led_index[0][0][-2])
+    if (led_index[0][0][-2] not in table.keys()):
+        table[led_index[0][0][-2]] = {}
+    table[led_index[0][0][-2]][0] = index
 
 def cb_rgb(points, led_rgb):
     print "RGB CALLBACK"
     print "Points: ",points
-	print "\nData" , led_rgb
-	rgb = led_rgb[0][0][-1]
-    print rgb
+    print "\nData" , led_rgb
+    rgbvalue = led_rgb[0][0][-1]
+    print rgbvalue
     now = int(time.time())
-	rgb[rgbpath]['Readings'] = [[now, pathrgb]]
-	print(led_rgb[0][0][-2])
-	if (led_rgb[0][0][-2] not in table.keys()):
-		table[led_rgb[0][0][-2]] = {}
-	table[led_rgb[0][0][-2]][1] = rgb
+    rgb[rgbpath]['Readings'] = [[now, rgbvalue]]
+    print(led_rgb[0][0][-2])
+    print(requests.post(archiveraddurl, data = json.dumps(rgb)))
+    if (led_rgb[0][0][-2] not in table.keys()):
+        table[led_rgb[0][0][-2]] = {}
+    table[led_rgb[0][0][-2]][1] = rgb
 
 def cb_show(points, led_show ):
     print "SHOW CALLBACK"
-	print "Points: ",points
-	print "\nData" , led_show
-	show = led_show[0][0][-1]
-	if (show == 1):
-		msg = {}
-		msg["type"] = "display"
-		print msg
-		msg_pack = msgpack.packb(msg)
-		sock.sendto(msg_pack, (UDP_IP, UDP_PORT))
+    print "Points: ",points
+    print "\nData" , led_show
+    show = led_show[0][0][-1]
+    if (show == 1):
+        msg = {}
+        msg["type"] = "display"
+        print msg
+        msg_pack = msgpack.packb(msg)
+        sock.sendto(msg_pack, (UDP_IP, UDP_PORT))
 
 def send_rgb():
-	print "Sending data to led strip"
-	print table
-	if (len(table.keys()) > 0):
-		earliest_time = min(table, key=table.get)
-		index = table[earliest_time][0]
-		rgb = table[earliest_time][1]
-		del table[earliest_time]
-		msg = {}
-		msg["type"] = "set"
-		msg["index"] = index
-		msg["rgb"] = rgb
-		print msg
-		msg_pack = msgpack.packb(msg)
-		print "sending"
-		print(sock.sendto(msg_pack, (UDP_IP, UDP_PORT)))
+    print "Sending data to led strip"
+    print table
+    if (len(table.keys()) > 0):
+        earliest_time = min(table, key=table.get)
+	print earliest_time
+        index = table[earliest_time][0]
+        rgb = table[earliest_time][1]
+        del table[earliest_time]
+        msg = {}
+        msg["type"] = "set"
+        msg["index"] = index
+        msg["rgb"] = rgb
+        print msg
+        msg_pack = msgpack.packb(msg)
+        print "sending"
+        print(sock.sendto(msg_pack, (UDP_IP, UDP_PORT)))
 
 r1 = RepublishClient(archiverurl, cb_index, restrict=subscription_index) 
 r2 = RepublishClient(archiverurl, cb_rgb, restrict=subscription_rgb)  
